49111be7f6795ba41c10f4dc98939bf4e99d8692
Mic <95868088+samplesamplemic@users.noreply.github.com>
2022-04-20 13:08:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+console.table({name: 'mic'})
+document.getElementById("demo").innerHTML = 'hack the planet';
+function buttononclick() {
+    var pagebutton = document.getElementById("demo");
+    pagebutton.click();
+}
