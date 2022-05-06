a1ec7d23c041eaf537579dc0ff7d1515d057dbfe
Mic <michelelt660@gmail.com>
2022-05-06 11:28:49 +0200

    proj

--- app.js
+++
@@ -1,6 +0,0 @@
-console.table({name: 'mic'})
-document.getElementById("demo").innerHTML = 'hack the planet';
-function buttononclick() {
-    var pagebutton = document.getElementById("demo");
-    pagebutton.click();
-}
